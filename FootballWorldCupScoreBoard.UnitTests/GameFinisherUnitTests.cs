e8baa3ab48e9a2d6814096be0c58609cd5645b8d
DayRM <dayris0817@gmail.com>
2021-01-28 17:22:38 +0100

    Adds GameFinisher implementation and GameFinisherUnitTests.

--- FootballWorldCupScoreBoard.UnitTests/GameFinisherUnitTests.cs
+++ FootballWorldCupScoreBoard.UnitTests/GameFinisherUnitTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FluentAssertions;
+using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +14,109 @@ namespace FootballWorldCupScoreBoard.UnitTests
     [Trait("Type", "Unit")]
     public class GameFinisherUnitTests
     {
+        [Fact]
+        public void Constructor_ScoreBoard_Null_Throws()
+        {
+            Action action = () => new GameFinisher(null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void FinishGame_Action_IsCalled()
+        {
+            var gameFinisherMock = new Mock<IGameFinisher<Game>>();
+            var gameFinisher = gameFinisherMock.Object;
+            gameFinisher.FinishGame(It.IsAny<Game>());
+            gameFinisherMock.Verify(m => m.FinishGame(It.IsAny<Game>()), Times.Once);
+        }
+
+
+        [Fact]
+        public void FinishGame_Game_Null_Throws()
+        {
+            var scoreBoard = Mock.Of<ScoreBoard>();
+            var gameFinisher = new GameFinisher(scoreBoard);
+            Action action = () => gameFinisher.FinishGame(null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void FinishGame_Remove_DecreaseCount()
+        {
+            var scoreBoard = ScoreBoardInitializer();
+
+            var countBeforeFinishGame = scoreBoard.Games.Count();
+
+            var game = new Game()
+            {
+                Id = 3,
+                HomeTeam = new HomeTeam() { Name = "Tarragona" },
+                AwayTeam = new AwayTeam() { Name = "Murcia" }
+            };
+
+            var GameFinisher = new GameFinisher(scoreBoard);
+
+            GameFinisher.FinishGame(game);
+
+
+            Assert.Equal(countBeforeFinishGame - 1, scoreBoard.Games.Count);
+        }
+
+        [Fact]
+        public void FinishGame_NotFoundGame_SameCount()
+        {
+            var scoreBoard = ScoreBoardInitializer();
+
+            var countBeforeFinishGame = scoreBoard.Games.Count();
+
+            var game = new Game()
+            {
+                Id = 5,
+                HomeTeam = new HomeTeam() { Name = "Alicante" },
+                AwayTeam = new AwayTeam() { Name = "Valencia" }
+            };
+
+            var GameFinisher = new GameFinisher(scoreBoard);
+
+            GameFinisher.FinishGame(game);
+
+
+            Assert.Equal(countBeforeFinishGame, scoreBoard.Games.Count);
+        }
+
+
+        private ScoreBoard ScoreBoardInitializer()
+        {
+            var scoreBoard = new ScoreBoard();
+
+            var game1 = new Game()
+            {
+                Id = 1,
+                HomeTeam = new HomeTeam() { Name = "Barcelona" },
+                AwayTeam = new AwayTeam() { Name = "Sevilla" }
+            };
+
+            var game2 = new Game()
+            {
+                Id = 2,
+                HomeTeam = new HomeTeam() { Name = "Madrid" },
+                AwayTeam = new AwayTeam() { Name = "Bilbao" }
+            };
+
+            var game3 = new Game()
+            {
+                Id = 3,
+                HomeTeam = new HomeTeam() { Name = "Tarragona" },
+                AwayTeam = new AwayTeam() { Name = "Murcia" }
+            };
+
+            scoreBoard.Games.Add(game1);
+            scoreBoard.Games.Add(game2);
+            scoreBoard.Games.Add(game3);
+
+            return scoreBoard;
+        }
+
+
     }
 }
--- FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameFinisher.cs
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameFinisher.cs
@@ -2,6 +2,7 @@
 using FootballWorldCupScoreBoard.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FootballWorldCupScoreBoard
@@ -9,9 +10,25 @@ namespace FootballWorldCupScoreBoard
     public class GameFinisher
         : IGameFinisher<Game>
     {
+
+        ScoreBoard ScoreBoard { get; set; }
+
+        public GameFinisher(ScoreBoard scoreBoard)
+        {
+            ScoreBoard = scoreBoard ?? throw new ArgumentNullException(nameof(scoreBoard));
+        }
+
+
         public void FinishGame(Game game)
         {
-            throw new NotImplementedException();
+            if (game == null) throw new ArgumentNullException(nameof(game));
+
+            var existingGame = ScoreBoard.Games.Where(x => x.Id == game.Id).FirstOrDefault();
+
+            if (existingGame != null)
+            {
+                ScoreBoard.Games.Remove(existingGame);
+            }
         }
     }
 }
