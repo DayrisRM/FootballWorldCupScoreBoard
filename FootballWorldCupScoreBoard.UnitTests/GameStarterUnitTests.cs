266ed704c979f58bf79dd0e1f0e5aa81b069fe78
DayRM <dayris0817@gmail.com>
2021-01-28 17:07:17 +0100

    Adds GameStarter implementation and Unit tests.

--- FootballWorldCupScoreBoard.UnitTests/GameStarterUnitTests.cs
+++ FootballWorldCupScoreBoard.UnitTests/GameStarterUnitTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FluentAssertions;
+using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +14,68 @@ namespace FootballWorldCupScoreBoard.UnitTests
     [Trait("Type", "Unit")]
     public class GameStarterUnitTests
     {
+        [Fact]
+        public void Constructor_ScoreBoard_Null_Throws()
+        {
+            Action action = () => new GameStarter(null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void StartGame_Action_IsCalled()
+        {
+            var starterGameMock = new Mock<IGameStarter<HomeTeam, AwayTeam>>();
+            var starterGame = starterGameMock.Object;
+            starterGame.StartGame(It.IsAny<HomeTeam>(), It.IsAny<AwayTeam>());
+            starterGameMock.Verify(m => m.StartGame(It.IsAny<HomeTeam>(), It.IsAny<AwayTeam>()), Times.Once);
+        }
+
+        [Fact]
+        public void StartGame_HomeTeam_Null_Throws()
+        {
+            var scoreBoard = Mock.Of<ScoreBoard>();
+            var GameStarter = new GameStarter(scoreBoard);
+            Action action = () => GameStarter.StartGame(null, It.IsAny<AwayTeam>());
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void StartGame_AwayTeam_Null_Throws()
+        {
+            var scoreBoard = Mock.Of<ScoreBoard>();
+            var GameStarter = new GameStarter(scoreBoard);
+            Action action = () => GameStarter.StartGame(It.IsAny<HomeTeam>(), null);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void StartGame_Add_ContainsGame()
+        {
+            var scoreBoard = new ScoreBoard();
+            var GameStarter = new GameStarter(scoreBoard);
+
+            var homeTeam = new HomeTeam() { Name = "Barcelona" };
+            var awayTeam = new AwayTeam() { Name = "Madrid" };
+
+            GameStarter.StartGame(homeTeam, awayTeam);
+
+            Assert.Single(scoreBoard.Games);
+        }
+
+        [Fact]
+        public void StartGame_Add_NotEmpty()
+        {
+            var scoreBoard = new ScoreBoard();
+            var GameStarter = new GameStarter(scoreBoard);
+
+            var homeTeam = new HomeTeam() { Name = "Barcelona" };
+            var awayTeam = new AwayTeam() { Name = "Madrid" };
+
+            GameStarter.StartGame(homeTeam, awayTeam);
+
+            Assert.NotEmpty(scoreBoard.Games);
+
+        }
+
     }
 }
--- FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameStarter.cs
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameStarter.cs
@@ -9,9 +9,29 @@ namespace FootballWorldCupScoreBoard
     public class GameStarter
         : IGameStarter<HomeTeam, AwayTeam>
     {
+        ScoreBoard ScoreBoard { get; set; }
+
+        public GameStarter(ScoreBoard scoreBoard)
+        {
+            ScoreBoard = scoreBoard ?? throw new ArgumentNullException(nameof(scoreBoard));
+        }
+
         public void StartGame(HomeTeam homeTeam, AwayTeam awayTeam)
         {
-            throw new NotImplementedException();
+            if (homeTeam == null) throw new ArgumentNullException(nameof(homeTeam));
+            if (awayTeam == null) throw new ArgumentNullException(nameof(awayTeam));
+
+            var gameId = ScoreBoard.Games.Count + 1; //TODO: MUST be entity id from DB
+
+            var game = new Game()
+            {
+                Id = gameId,
+                HomeTeam = homeTeam,
+                AwayTeam = awayTeam,
+                Score = new Score()
+            };
+
+            ScoreBoard.Games.Add(game);
         }
     }
 }
