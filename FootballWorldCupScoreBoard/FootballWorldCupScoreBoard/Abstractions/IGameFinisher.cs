08f480c50e9769d393187b070e68720f534cabb6
DayRM <dayris0817@gmail.com>
2021-01-28 17:14:25 +0100

    Adds IGameFinisher, GameFinisher implementation and GameFinisherUnitTests.

---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/Abstractions/IGameFinisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard.Abstractions
+{
+    public interface IGameFinisher<TInput>
+    {
+        void FinishGame(TInput game);
+    }
+}
---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameFinisher.cs
@@ -0,0 +1,17 @@
+﻿using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard
+{
+    public class GameFinisher
+        : IGameFinisher<Game>
+    {
+        public void FinishGame(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
