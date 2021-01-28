4bc95b46377dc00124ef007cb3725d1471807de2
DayRM <dayris0817@gmail.com>
2021-01-28 18:45:37 +0100

    Adds ISummarySupplier, SummarySupplier implementation and SummarySupplierUnitTests.

---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/Abstractions/ISummarySupplier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard.Abstractions
+{
+    public interface ISummarySupplier<TOutput>
+    {
+        TOutput GetSummary();
+    }
+}
---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/SummarySupplier.cs
@@ -0,0 +1,17 @@
+﻿using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard
+{
+    public class SummarySupplier
+         : ISummarySupplier<List<Game>>
+    {
+        public List<Game> GetSummary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
