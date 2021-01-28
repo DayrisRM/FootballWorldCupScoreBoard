1f3fcfb75fc9175a06d0e87c486b1d61ee709b34
DayRM <dayris0817@gmail.com>
2021-01-28 17:42:45 +0100

    Adds IScoreUpdater, ScoreUpdater implementation, ScoreUpdaterUnitTests.

---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/Abstractions/IScoreUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard.Abstractions
+{
+    public interface IScoreUpdater<TInput, TInput2, TInput3>
+    {
+        void UpdateScore(TInput game, TInput2 scoreHomeTeam, TInput3 scoreAwayTeam);
+    }
+}
---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/ScoreUpdater.cs
@@ -0,0 +1,17 @@
+﻿using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard
+{
+    public class ScoreUpdater
+        : IScoreUpdater<Game, int?, int?>
+    {
+        public void UpdateScore(Game game, int? scoreHomeTeam, int? scoreAwayTeam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
