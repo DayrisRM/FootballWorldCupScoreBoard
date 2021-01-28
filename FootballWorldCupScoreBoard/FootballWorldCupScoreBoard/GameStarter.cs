a09ac7318f9798cc7be72d9170c3decc1b3e79e5
DayRM <dayris0817@gmail.com>
2021-01-28 16:56:56 +0100

    Adds IGameStarter, GameStarter implementation and GameStarterUnitTests.

---
+++ FootballWorldCupScoreBoard/FootballWorldCupScoreBoard/GameStarter.cs
@@ -0,0 +1,17 @@
+﻿using FootballWorldCupScoreBoard.Abstractions;
+using FootballWorldCupScoreBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballWorldCupScoreBoard
+{
+    public class GameStarter
+        : IGameStarter<HomeTeam, AwayTeam>
+    {
+        public void StartGame(HomeTeam homeTeam, AwayTeam awayTeam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
